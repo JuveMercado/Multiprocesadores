b6db65abbe4e7db358f41c378e4f27050b3f9387
Juve Mercado <a01730700@itesm.mx>
2021-09-03 22:31:33 -0500

    Actividad 1.6

--- Actividad 1.6/secuencial.c
+++ Actividad 1.6/secuencial.c
@@ -6,8 +6,8 @@ void runge1(int n,  int rhe, char d[]){
 	FILE *fptr;
 
 	fptr=fopen(d,"w");
-	printf("Numero de pasos:%d Atendido por thread:%d\n", n,rhe);
-	fprintf(fptr, "Datos que encuentra el metodo de Euler(variable ind.\t variable dep.\t numero de thread)\n");
+	printf("Pasos:%d Thread:%d\n", n,rhe);
+	fprintf(fptr, "Datos Euler");
 	double h,t,w,ab,k1,k2,k3,k4;
 	double w0=M_PI/4,a=0,b=M_PI;
 	int i;
@@ -23,7 +23,7 @@ void runge1(int n,  int rhe, char d[]){
 		k4= h*((t+h)*exp(3*(t+h))-2*(w+k3));
 		w=w+(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
 		fprintf(fptr, "%f\t %f\n", t, w);
-		//fprintf(fptr, "%f\t %f \t numero de thread:%d\n", t+h, w,rhe/*omp_get_thread_num()*/);
+		
 	}
 	fclose(fptr);
 }
@@ -32,8 +32,8 @@ void runge2(int n,  int rhe, char d[]){
 	FILE *fptr;
 
 	fptr=fopen(d,"w");
-	printf("Numero de pasos:%d Atendido por thread:%d\n", n,rhe);
-	fprintf(fptr, "Datos que encuentra el metodo de Euler(variable ind.\t variable dep.\t numero de thread)\n");
+	printf("Pasos:%d Thread:%d\n", n,rhe);
+	fprintf(fptr, "Datos Euler");
 	double h,t,w,ab,k1,k2,k3,k4;
 	double w0=M_PI/4,a=0,b=M_PI;
 	int i;
@@ -49,7 +49,7 @@ void runge2(int n,  int rhe, char d[]){
 		k4= h*(1+pow((t+h)-(w+k3),2));
 		w=w+(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
 		fprintf(fptr, "%f\t %f\n", t, w);
-		//fprintf(fptr, "%f\t %f \t numero de thread:%d\n", t+h, w,rhe/*omp_get_thread_num()*/);
+		
 	}
 	fclose(fptr);
 }
@@ -58,8 +58,8 @@ void runge3(int n,  int rhe, char d[]){
 	FILE *fptr;
 
 	fptr=fopen(d,"w");
-	printf("Numero de pasos:%d Atendido por thread:%d\n", n,rhe);
-	fprintf(fptr, "Datos que encuentra el metodo de Euler(variable ind.\t variable dep.\t numero de thread)\n");
+	printf("Pasos:%d Thread:%d\n", n,rhe);
+	fprintf(fptr, "Datos Euler");
 	double h,t,w,ab,k1,k2,k3,k4;
 	double w0=M_PI/4,a=0,b=M_PI;
 	int i;
@@ -75,7 +75,7 @@ void runge3(int n,  int rhe, char d[]){
 		k4= h*(1+(t+h)/(w+k3));
 		w=w+(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
 		fprintf(fptr, "%f\t %f\n", t, w);
-		//fprintf(fptr, "%f\t %f \t numero de thread:%d\n", t+h, w,rhe/*omp_get_thread_num()*/);
+		
 	}
 	fclose(fptr);
 }
@@ -85,8 +85,8 @@ void runge4(int n,  int rhe, char d[]){
 	FILE *fptr;
 
 	fptr=fopen(d,"w");
-	printf("Numero de pasos:%d Atendido por thread:%d\n", n,rhe);
-	fprintf(fptr, "Datos que encuentra el metodo de Euler(variable ind.\t variable dep.\t numero de thread)\n");
+	printf("Pasos:%d Thread:%d\n", n,rhe);
+	fprintf(fptr, "Datos Euler");
 	double h,t,w,ab,k1,k2,k3,k4;
 	double w0=M_PI/4,a=0,b=M_PI;
 	int i;
@@ -102,7 +102,7 @@ void runge4(int n,  int rhe, char d[]){
 		k4= h*(cos(2*(t+h)*(w+k3))+sin(3*(t+h)*(w+k3)));
 		w=w+(1.0/6.0)*(k1+2.0*k2+2.0*k3+k4);
 		fprintf(fptr, "%f\t %f\n", t, w);
-		//fprintf(fptr, "%f\t %f \t numero de thread:%d\n", t+h, w,rhe/*omp_get_thread_num()*/);
+		
 	}
 	fclose(fptr);
 }
@@ -111,10 +111,10 @@ void main(int argc, char const *argv[])
 {
 	const double start = omp_get_wtime();
 	int N = 50000;
-	runge1(N, 1, "runge_ns_1.txt");
-	runge2(N, 2, "runge_ns_2.txt");
-	runge3(N, 3, "runge_ns_3.txt");
-	runge4(N, 4, "runge_ns_4.txt");
+	runge1(N, 1, "Secuencial_1.txt");
+	runge2(N, 2, "Secuencial_2.txt");
+	runge3(N, 3, "Secuencial_3.txt");
+	runge4(N, 4, "Secuencial_4.txt");
 	const double end = omp_get_wtime();
-	printf("tomo (%lf) segundos\n", (end - start));
+	printf("Segundos: %lf\n", (end - start));
 }
